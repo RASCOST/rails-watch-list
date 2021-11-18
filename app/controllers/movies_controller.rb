1566cb459a5a2483e7acda1a3526baec874718cb
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-18 14:35:03 +0100

    movies action new and create, view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,22 @@ class MoviesController < ApplicationController
   end
 
   def new
-
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new(movie_params)
 
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
   end
 end
